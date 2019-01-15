4c161f2c638fd90495a41bb1d2c5ed0902bba49f
Xu <xumeng14@qq.com>
2019-01-15 17:03:53 +0800

    实现下拉刷新

--- pages/list/list.js
+++ pages/list/list.js
@@ -7,9 +7,26 @@ Page({
    */
   data: {
     categories:{},//当前全部分类
-    shops:[]//分类下的全部店铺
-  },
+    shops:[],//分类下的全部店铺
+    pageindex:0,
+    pageSize:20,
+    totalCount:0,
+    hasmore:true
 
+
+  },
+  //自定义加载更多函数
+  loadmore(){
+    let { pageindex,pageSize,searchText }=this.data
+    const params = { _page: ++pageindex, limit: pageSize }
+    if (searchText) params.q=searchText
+    fetch(`categories/${this.data.categories.id}/shops`, params).then(res => {
+      const totalCount = parseInt(res.header['X-Total-Count'])
+      const hasmore=this.data.pageindex*this.data.pageSize<totalCount
+      const shops=this.data.shops.concat(res.data)
+      this.setData({ shops,pageindex,totalCount,hasmore })
+     })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -18,16 +35,9 @@ Page({
       this.setData({categories:res.data})
       wx.setNavigationBarTitle({title:res.data.name})
        //加载完商铺信息过后再去加载分类信息
-      return fetch(`categories/${options.cat}/shops`,{_page:1,limit:10})
+      this.loadmore()
     })
-    .then(res=>{
-      console.log(res.data)
-      this.setData({ shops: res.data })
-    })
-     
-
-
-  },
+ },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -40,39 +50,20 @@ Page({
     
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
+  
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+    console.log("shangla")
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+    //  console.log("到底了")
+    this.loadmore()
   },
 
   /**
